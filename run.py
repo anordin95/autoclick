ee02433888fe1aa064a97ee4ad19ab7c777398b5
Alexander Nordin <anordin@butterflynetinc.com>
2020-04-11 00:02:59 -0400

    randomly move mouse sometimes. sometimes update click position with margin for error. smooth movements

--- run.py
+++ run.py
@@ -4,6 +4,46 @@ from getkey import getkey
 import random
 import argparse
 
+class ClickLocation:
+
+	MARGIN_FOR_ERROR_PX = 3
+	CHANCE_TO_PERTURBATE = 0.05
+
+	def __init__(self, original_location):
+		'''
+		original_location: input at the beginning of the script. should be centered on click item
+		current_location: the script's current notion of the location
+		'''
+		self.original_location = original_location
+		self.current_location = original_location
+
+	def maybe_slightly_perturbate():
+		'''
+		perturbate location, but ensure it stays within a 
+		margin for error of the original location
+		'''		
+		if random.random() >= self.CHANCE_TO_PERTURBATE:
+			return
+		
+		print("Perturbating location...")
+
+		original_x = self.original_location.x
+		original_y = self.original_location.y
+
+		lower_bound_x = original_x - self.MARGIN_FOR_ERROR_PX
+		upper_bound_x = original_x + self.MARGIN_FOR_ERROR_PX
+		new_x = random.randint(lower_bound_x, upper_bound_x)
+
+		lower_bound_y = original_y - self.MARGIN_FOR_ERROR_PX
+		upper_bound_y = original_y + self.MARGIN_FOR_ERROR_PX
+		new_y = random.randint(lower_bound_y, upper_bound_y)
+
+		self.current_location = pyautogui.Point(new_x, new_y)
+
+	def get_current_loc():
+		return self.current_location
+
+
 def setup_click_location():
 	print(f"Setting up click location."
 		f" Ensure this program remains in the computer's context."
@@ -17,7 +57,7 @@ def setup_click_location():
 
 	click_location = pyautogui.position()
 
-	return click_location
+	return ClickLocation(click_location)
 
 def get_sleep_time():
 	# distribution params
@@ -29,7 +69,7 @@ def get_sleep_time():
 	sigma = random.choice(sigmas)
 
 	# simulate human taking a break
-	break_probability = 0.005
+	break_probability = 0.003 
 	mu_break = 45
 	sigma_break = 10
 	if random.random() < break_probability:
@@ -40,19 +80,44 @@ def get_sleep_time():
 	# ensure no negative values are sent back.
 	return max(random.gauss(mu, sigma), 0)
 
+def maybe_randomly_move_mouse(click_location):
+	def _move_mouse_randomly():
+		num_movements = random.randint(1,8)
+		for _ in range(num_movements):
+			x_distance_to_move = random.gauss(mu=0, sigma=200)
+			y_distance_to_move = random.gauss(mu=0, sigma=200)
+			pyautogui.move(x_distance_to_move, 
+							y_distance_to_move,
+							duration = random.gauss(1.5, 0.3),
+							tween=pyautogui.easeInOutQuad)
+
+	p_random_mouse_move = 0.1
+	
+	if random.random() < p_random_mouse_move:
+		print("Moving mouse randomly...")
+		_move_mouse_randomly()
+
+		pyautogui.moveTo(click_location.get_current_loc(), duration = random.gauss(1.5, 0.3), tween=pyautogui.easeInOutQuad)
+		# slowly return mouse to original click location
+
 def continuously_click(click_location, runtime):
 	print(f"Continously clicking for {runtime} minutes...")
 	start = time.time()
 	while True:
 		if (time.time() - start) > runtime * 60:
+			print("Finishing...")
 			break
 
 		random_time = get_sleep_time()
 		print(f"Sleeping for {random_time}s.")
 		time.sleep(random_time)
+
+		maybe_randomly_move_mouse(click_location)
+		
+		click_location.maybe_slightly_perturbate()
 		
 		print("Clicking!")
-		pyautogui.click(click_location)
+		pyautogui.click(click_location.get_current_loc())
 
 def run(runtime):
 	click_location = setup_click_location()
